0c6e66a477b326290f860fb27f08e8d673e5e53e
ranadheerreddy <ranadheerreddy904@gmail.com>
2020-01-18 10:40:03 +0530

    final commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { ListComponent } from './list/list.component';
 import { DataService } from './data.service';
+import { SearchPipe } from './search.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    ListComponent
+    ListComponent, SearchPipe
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
