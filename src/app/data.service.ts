90251219a287c513f616f77516da76b4372a0ce9
Git Server Admin <admin@git-server>
2020-01-18 04:21:57 +0000

    Add initial repository

---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+@Injectable()
+export class DataService {
+
+  private vegetableNameSource = new BehaviorSubject<string>("");
+  currentVegetableName = this.vegetableNameSource.asObservable();
+
+  constructor() { }
+
+  changeVegetableName(vegetable: string) {
+    // add data to an observable
+  }
+}
